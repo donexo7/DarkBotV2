db8f4cf6dd86788784be23e947c94d21ecf1fc2b
donexo7 <100534879+donexo7@users.noreply.github.com>
2022-03-16 10:39:56 -0600

    Update play6.js

--- plugins/play6.js
+++ plugins/play6.js
@@ -3,6 +3,6 @@ let handler = async (m, { conn, usedPrefix, command, text }) => {
 if (!text) throw `*Formato de uso: ${usedPrefix + command} Nombre de la canción o video*\n*Ejemplo:*\n*${usedPrefix + command} Billie Eilish bored*`
 let res = await fetch("https://api-alc.herokuapp.com/api/download/play-mp4?query="+text+"&apikey=ConfuMods")
 let json = await res.json()
-conn.sendFile(m.chat, json.link, 'error.mp4', `*¡Aqui tienes el video!*\n🐈 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 🐈`, m)}
+conn.sendFile(m.chat, json.link, 'error.mp4', `*¡Aqui tienes el video!*\n💀 Exo 💀`, m)}
 handler.command = /^(play6)$/i
 module.exports = handler
